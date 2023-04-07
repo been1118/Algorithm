7e4dc4104fa9e7ca13af82a2d158fe1d6bfa7609
Sindonghyun04 <123082067+been1118@users.noreply.github.com>
2023-04-08 00:51:48 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        if(num == 1) return 0;
+        for(int i = 1; i<=500; i++){
+           num = num%2 == 0 ? num/2 : num*3 + 1;
+           if(num == 1)return i;
+        }
+        return -1;
+    }
+}
