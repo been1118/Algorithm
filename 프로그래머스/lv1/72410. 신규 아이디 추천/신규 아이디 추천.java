cbac026ce361b0ebc40196fe349998d53dba637a
Sindonghyun04 <123082067+been1118@users.noreply.github.com>
2023-04-09 01:12:51 +0900

    [level 1] Title: 신규 아이디 추천, Time: 7.16 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,27 +1,30 @@
 class Solution {
     public String solution(String new_id) {
-        String answer = "";
         new_id = new_id.toLowerCase();
-        new_id = new_id.replaceAll("[!@#*$%^&+=]","");
-        new_id = new_id.replaceAll("\\.+", ".");
-        if (new_id.length() > 0 && new_id.charAt(0) == '.') 
+        new_id = new_id.replaceAll("[^\\.|\\-|\\w]","");
+        new_id = new_id.replaceAll("[/\\./]{2,}", ".");
+        if (new_id.equals("."))
+            new_id = "";
+
+        if (new_id.length() > 1 && new_id.charAt(0) == '.')
             new_id = new_id.substring(1);
-        if (new_id.length() > 0 && new_id.charAt(new_id.length() - 1) == '.') 
-            new_id = new_id.substring(0, new_id.length() - 1);
+
         if (new_id.length() > 15) 
             new_id = new_id.substring(0, 15);
-        if (new_id.length() > 0 && new_id.charAt(0) == '.') 
-            new_id = new_id.substring(1);
-        if (new_id.length() > 0 && new_id.charAt(new_id.length() - 1) == '.') 
+
+        if (new_id.length() > 1 && new_id.charAt(new_id.length() - 1) == '.') 
             new_id = new_id.substring(0, new_id.length() - 1);
-        if(new_id.equals("")) new_id = "a";
+
+        if(new_id.length() == 0) new_id = "a";
+
         String last = new_id.charAt(new_id.length()-1) + "";
+
         if (new_id.length() == 2) {
             new_id += last;
             }else if(new_id.length() == 1){
             new_id += last + last;
         }
-        
         return new_id;
     }
 }
+
