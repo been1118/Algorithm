36b1db9132af1c7332eef25d099e6795db970c64
Sindonghyun04 <123082067+been1118@users.noreply.github.com>
2023-04-11 23:58:39 +0900

    [level 1] Title: 체육복, Time: 0.37 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -1,30 +1,30 @@
+import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n - lost.length;
-        boolean[] visited = new boolean[lost.length];
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(lost[i] == reserve[j]){
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+                
+            }
+        }
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(lost[i]-1 == reserve[j] || lost[i]+1 == reserve[j]){
+                    answer++;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
         
-        for(int i=0; i<reserve.length; i++){
-            for(int j=0; j<lost.length; j++){
-                if(!visited[j] && reserve[i] == lost[j]){
-                    visited[j] = true;
-                    answer++;
-                    break;
-                }
-            }
-        }
-        for(int i=0; i<reserve.length; i++){
-            for(int j=0; j<lost.length; j++){
-                if(!visited[j] && reserve[i]+1 == lost[j]){
-                    visited[j] = true;
-                    answer++;
-                    break;
-                }else if(!visited[j] && reserve[i]-1 == lost[j]){
-                    visited[j] = true;
-                    answer++;
-                    break;
-                }
-            }
-        }
         
         return answer;
     }
