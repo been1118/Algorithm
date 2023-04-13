f0dd698c0d3c7338f48d395584207431d9b11df4
Sindonghyun04 <123082067+been1118@users.noreply.github.com>
2023-04-13 23:47:09 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,6 +1,30 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {};
+        int min = 0;
+        int max = 0;
+        int cnt = 0;
+        int cnt2 = 0;
+        for(int zero : lottos){
+            if(zero == 0) cnt++;
+        }
+        for(int i=0; i<lottos.length; i++){
+            for(int j=0; j<win_nums.length; j++){
+                if(lottos[i] == win_nums[j])cnt2++;
+            }
+        }
+        max = (cnt2+cnt)>1 ? 7-(cnt2+cnt) : 6;
+        min = cnt2 > 1 ? 7-cnt2 : 6;
+        int[] answer ={max, min};
         return answer;
     }
 }
+
+
+
+
+
+
+
+
+//로또 1등 당첨되고 싶다.
+//진심이다..
