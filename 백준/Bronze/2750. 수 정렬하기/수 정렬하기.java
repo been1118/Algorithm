b31271309f1fd13156a3a8c887ecac79e6d1970b
신동현 <123082067+been1118@users.noreply.github.com>
2023-07-06 17:52:13 +0900

    [Bronze II] Title: 수 정렬하기, Time: 188 ms, Memory: 16548 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,29 +1,27 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int a = Integer.parseInt(br.readLine());
-        List<Integer> integers = new LinkedList<>();
+        int[] list = new int[a];
         for (int i = 0; i < a; i++) {
             int b = Integer.parseInt(br.readLine());
-            integers.add(b);
+            list[i] = b;
         }
         int t;
-        for (int i = 0; i < integers.size(); i++) {
-            for (int j = 0; j < integers.size()-1; j++) {
-                if (integers.get(j) < integers.get(j + 1)) {
-                    t = integers.get(j);
-                    integers.set(j, integers.get(j + 1));
-                    integers.set(j + 1, t);
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a - 1; j++) {
+                if (list[j] < list[j + 1]) {
+                    t = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = t;
                 }
             }
         }
-        for (int i = integers.size()-1; i >= 0; i--) {
-            bw.write(integers.get(i) + "\n");
+        for (int i = a - 1; i >= 0; i--) {
+            bw.write(list[i] + "\n");
         }
         bw.flush();
         bw.close();
