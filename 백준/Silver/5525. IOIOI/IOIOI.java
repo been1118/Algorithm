c73c9dff278169cbf8261055697c6cab6d836369
신동현 <123082067+been1118@users.noreply.github.com>
2023-07-18 00:25:42 +0900

    [Silver I] Title: IOIOI, Time: 272 ms, Memory: 22096 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        char[] s = br.readLine().toCharArray();
+        int result = 0;
+        int count = 0;
+
+        for (int i = 1; i < M - 1; i++) {
+            if (s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+                count++;
+                if (count == N) {
+                    count--;
+                    result++;
+                }
+                i++;
+            } else {
+                count = 0;
+            }
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+
+}
