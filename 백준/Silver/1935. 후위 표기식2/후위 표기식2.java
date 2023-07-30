95c15bdc8f831227785b6c1d0b3022e0527ce7a8
신동현 <123082067+been1118@users.noreply.github.com>
2023-07-30 14:13:18 +0900

    [Silver III] Title: 후위 표기식2, Time: 136 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public int number;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        String data = br.readLine();
+        double[] arr = new double[num];
+        for(int i = 0; i < num; i++) {
+            arr[i] = Double.parseDouble(br.readLine());
+        }
+
+        Stack<Double> stack = new Stack<>();
+
+        double result = 0;
+        for(int i = 0; i < data.length(); i++) {
+            if('A' <= data.charAt(i) && data.charAt(i) <= 'Z') {
+                stack.push(arr[data.charAt(i) - 'A']);
+            } else {
+                if(!stack.isEmpty()) {
+                    double first = stack.pop();
+                    double second = stack.pop();
+                    switch (data.charAt(i)) {
+                        case '+':
+                            result = second + first;
+                            stack.push(result);
+                            continue;
+                        case '-':
+                            result = second - first;
+                            stack.push(result);
+                            continue;
+                        case '*':
+                            result = second * first;
+                            stack.push(result);
+                            continue;
+                        case '/':
+                            result = second / first;
+                            stack.push(result);
+                    }
+                }
+            }
+        }
+
+        System.out.printf("%.2f", stack.pop());
+    }
+}
