aba4c7d4e1fd62b56e9f11023cef39be423cb0b4
신동현 <123082067+been1118@users.noreply.github.com>
2023-07-31 04:19:24 +0900

    [Silver V] Title: 부분 문자열, Time: 192 ms, Memory: 17432 KB -BaekjoonHub

---
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            st = new StringTokenizer(str);
+            Queue<Character> q = new LinkedList<>();
+
+            String s1 = st.nextToken();
+            String s2 = st.nextToken();
+
+            for (int i = 0; i < s1.length(); i++) {
+                q.add(s1.charAt(i));
+            }
+
+            for (int i = 0; i < s2.length(); i++) {
+                if (s2.charAt(i) == q.peek()) {
+                    q.poll();
+                    if (q.isEmpty()) {
+                        System.out.println("Yes");
+                        break;
+                    }
+                }
+            }
+
+            if (!q.isEmpty()) System.out.println("No");
+        }
+    }
+}
