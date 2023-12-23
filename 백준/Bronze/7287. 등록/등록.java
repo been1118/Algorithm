8c3fb1243322b94b41ad93e3fc191c70063ca9a6
신동현 <123082067+been1118@users.noreply.github.com>
2023-12-23 21:06:15 +0900

    [Bronze V] Title: 등록, Time: 112 ms, Memory: 13952 KB -BaekjoonHub

--- 백준/Bronze/7287. 등록/등록.java
+++ 백준/Bronze/7287. 등록/등록.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        bw.write("127" + "\n" + "a9150363a");
+        bw.write("128\na9150363a");
 
         bw.flush();
         bw.close();
