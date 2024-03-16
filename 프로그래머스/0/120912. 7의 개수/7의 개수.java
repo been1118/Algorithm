0fffa2a1e594de24d5c821b2a8865a5ea0dacb69
신동현 <123082067+been1118@users.noreply.github.com>
2024-03-16 17:03:59 +0900

    [level 0] Title: 7의 개수, Time: 0.06 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for (int i : array) {
+            String str = String.valueOf(i);
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '7') answer++; 
+            }
+        }
+        return answer;
+    }
+}
