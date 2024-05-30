841f3cb9b098ae6c5157fa9ed12f3c9729fe4a5f
신동현 <123082067+been1118@users.noreply.github.com>
2024-05-30 23:11:34 +0900

    [Silver IV] Title: 한수, Time: 128 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -9,23 +9,18 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         int answer = 0;
-        for (int i = 1; i <= N; i++) {
-            if (N < 100) {
-                answer = N;
-                break;
-            } else {
-                answer = 99;
-                for (int j = 100; j <= N; j++) {
-                    String str = String.valueOf(j);
-                    if (str.charAt(0) - str.charAt(1) == str.charAt(1) - str.charAt(2)) {
-                        answer++;
-                    }
+        if (N < 100) {
+            answer = N;
+        } else {
+            answer = 99;
+            for (int j = 100; j <= N; j++) {
+                String str = String.valueOf(j);
+                if ((str.charAt(0) - str.charAt(1)) == (str.charAt(1) - str.charAt(2))) {
+                    answer++;
                 }
             }
-
-
         }
-
+        
         System.out.println(answer);
     }
 }
