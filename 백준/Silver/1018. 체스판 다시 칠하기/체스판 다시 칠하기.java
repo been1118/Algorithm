cba18c43e5a272d14f34ec7cb54fb75be85897ca
신동현 <123082067+been1118@users.noreply.github.com>
2024-05-30 22:37:47 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 128 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,48 +1,44 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        boolean[][] cp = new boolean[N][M];
+
+        boolean[][] arr = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
-            String tf = br.readLine();
+            String str = br.readLine();
             for (int j = 0; j < M; j++) {
-                if (tf.charAt(j) == 'W') cp[i][j] = true;
-                else cp[i][j] = false;
+                arr[i][j] = str.charAt(j) == 'B';
             }
         }
 
-        int N_row = N - 7;
-        int M_col = M - 7;
-        int cnt = 64;
-        for (int i = 0; i < N_row; i++) {
-            for (int j = 0; j < M_col; j++) {
-                int end_X = i + 8;
-                int end_Y = j + 8;
-                int nCnt = 0;
-                boolean tf = cp[i][j];
+        int answer = 64;
 
-                for (int k = i; k < end_X; k++) {
-                    for (int l = j; l < end_Y; l++) {
-                        if (cp[k][l] != tf) nCnt++;
+        for (int i = 0; i < N - 7; i++) {
+            for (int j = 0; j < M - 7; j++) {
+                int cnt = 0;
+                boolean tf = arr[i][j];
+
+                for (int k = i; k < i + 8; k++) {
+                    for (int l = j; l < j + 8; l++) {
+                        if (arr[k][l] != tf) cnt++;
                         tf = !tf;
                     }
                     tf = !tf;
                 }
-                nCnt = Math.min(nCnt, 64 - nCnt);
-                cnt = Math.min(nCnt, cnt);
+                cnt = Math.min(cnt, 64 - cnt);
+                answer = Math.min(answer, cnt);
             }
         }
-        bw.write(String.valueOf(cnt));
-        bw.flush();
-        bw.close();
+
+        System.out.println(answer);
     }
 }
