7dad828e6352e089e50ba46e11bd4509ff7a80a4
신동현 <123082067+been1118@users.noreply.github.com>
2024-06-06 21:29:58 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 724 ms, Memory: 50016 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,31 +1,28 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int N = Integer.parseInt(br.readLine());
-
-        int[][] list = new int[N][2];
+        int[][] arr = new int[N][2];
+        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            list[i][0] = a;
-            list[i][1] = b;
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(list, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0]!=o2[0] ? o1[0]-o2[0] : o1[1]-o2[1];
-            }
 
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) return o1[1] - o2[1];
+            else return o1[0] - o2[0];
         });
+
         for (int i = 0; i < N; i++) {
-            bw.write(list[i][0] + " " + list[i][1] + "\n");
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
         }
         bw.flush();
         bw.close();
