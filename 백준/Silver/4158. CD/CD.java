ea47f6935f210fdf6e6c3e5022e09d1a5756007b
신동현 <123082067+been1118@users.noreply.github.com>
2024-06-27 21:38:05 +0900

    [Silver V] Title: CD, Time: 940 ms, Memory: 278116 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.java
+++ 백준/Silver/4158. CD/CD.java
@@ -1,33 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        HashSet<Integer> set;
         while (true) {
-            int cnt = 0;
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
             if (N == 0 && M == 0) break;
 
-            set = new HashSet<>();
+            int[] nArr = new int[N];
+            int[] mArr = new int[M];
 
             for (int i = 0; i < N; i++) {
-                set.add(Integer.parseInt(br.readLine()));
+                nArr[i] = Integer.parseInt(br.readLine());
             }
 
             for (int i = 0; i < M; i++) {
-                if (set.contains(Integer.parseInt(br.readLine()))) cnt++;
+                mArr[i] = Integer.parseInt(br.readLine());
+            }
+
+            int i = 0, j = 0, cnt = 0;
+            while (N > i && M > j) {
+                if (nArr[i] == mArr[j]) {
+                    cnt++;
+                    i++;
+                    j++;
+                } else if (nArr[i] > mArr[j]) {
+                    j++;
+                } else {
+                    i++;
+                }
             }
             System.out.println(cnt);
         }
-
     }
 }
