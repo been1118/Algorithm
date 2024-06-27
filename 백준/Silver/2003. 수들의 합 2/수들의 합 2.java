35728619e33889f676bff23ffe6be2cdba125e6f
신동현 <123082067+been1118@users.noreply.github.com>
2024-06-27 22:37:37 +0900

    [Silver IV] Title: 수들의 합 2, Time: 132 ms, Memory: 14912 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,41 +1,30 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] intList = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            intList[i] = Integer.parseInt(st.nextToken());
-        }
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            int num = intList[i];
-            for (int j = i + 1; j < N + 1; j++) {
-                if (num == M) {
-                    cnt++;
-                    break;
-                }
-                if (j == N) break;
-                num += intList[j];
-                if (num == M) {
-                    cnt++;
-                    break;
-                } else if (num > M) {
-                    break;
-                }
-            }
-        }
-        bw.write(String.valueOf(cnt));
-        bw.flush();
-        bw.close();
-    }
 
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int i = 0, j = 0, cnt = 0;
+        int sum = 0;
+        while (true) {
+            if (sum >= M) sum -= arr[i++];
+            else if (j == N) break;
+            else sum += arr[j++];
+            if (sum == M) cnt++;
+        }
+        System.out.println(cnt);
+    }
 }
