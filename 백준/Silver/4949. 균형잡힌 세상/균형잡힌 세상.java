aa47ff3f9180c152a9d9a6a04b16519c65edfecc
신동현 <123082067+been1118@users.noreply.github.com>
2024-07-21 21:26:20 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 220 ms, Memory: 19976 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -5,37 +5,43 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        while(true) {
+        Stack<Character> stack;
+        loop1 : while(true) {
             String str = br.readLine();
             if (str.equals(".")) break;
-            bw.write(yesOrNo(str) + "\n");
+            if (!str.endsWith(".")) {
+                bw.write("no\n");
+                continue;
+            }
+
+            stack = new Stack<>();
+            for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+
+                if (ch == '(') stack.push('(');
+                if (ch == ')') {
+                    if (stack.empty() || stack.peek() != '(') {
+                        bw.write("no\n");
+                        continue loop1;
+                    } else {
+                        stack.pop();
+                    }
+                }
+
+                if (ch == '[') stack.push('[');
+                if (ch == ']') {
+                    if (stack.empty() || stack.peek() != '[') {
+                        bw.write("no\n");
+                        continue loop1;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+            if (stack.empty()) bw.write("yes\n");
+            else bw.write("no\n");
         }
         bw.flush();
         bw.close();
-    }
-
-    public static String yesOrNo(String str) {
-        if (!str.endsWith(".")) return "no";
-
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-
-            if (ch == '(') stack.push('(');
-            if (ch == ')') {
-                if (stack.empty() || stack.peek() != '(') {
-                    return "no";
-                } else stack.pop();
-            }
-
-            if (ch == '[') stack.push('[');
-            if (ch == ']') {
-                if (stack.empty() || stack.peek() != '[') {
-                    return "no";
-                } else stack.pop();
-            }
-        }
-        if (stack.empty()) return "yes";
-        else return "no";
     }
 }
